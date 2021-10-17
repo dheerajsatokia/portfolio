21d0c12443b8e3acd9083b258400f6c2d96d15b0
dheeraj satokia <dheerajsatokia@gmail.com>
2021-10-17 23:55:43 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-34ba63b0a1ab9a6f5957.js"
+    "url": "webpack-runtime-303ddb419c74a54863c9.js"
   },
   {
     "url": "framework-305b3707783ccc9d7ca6.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b9d4779121eae15e9bac74edf2ac3c29"
+    "revision": "948ff630dea87ba45071ec7d4de32e52"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-16703ee5599528db9f93.js"
